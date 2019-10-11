cab882b974a6d87ba8e43d54bab7434466142abc
atticusg <atticusg@gmail.com>
2019-10-10 17:15:27 -0700

    Does hyperparamter search now

--- train.py
+++ train.py
@@ -3,22 +3,33 @@ import nli
 from tf_shallow_neural_classifier import TfShallowNeuralClassifier
 import os
 import random
+import tensorflow as tf
+import itertools
 
+hidden_dims = [50,100,200]
+max_iters = [5,10,20]
+vocab_dims= [50]
+activations = [tf.nn.relu, tf.nn.tanh]
+etas = [0.01, 0.001, 0.0001]
+hyps = itertools.product(hidden_dims, max_iters, vocab_dims, activations, etas)
 best_embeddings = ""
 bestF1 = 0
-for hyp in [[0], [1]]:
+best_params = None
+for hyp in [[100,20, 50, tf.nn.relu, 0.001], [100,20, 50, tf.nn.relu, 0.001]]:
     hypname = ""
     for param in hyp:
         hypname += str(param)
     train, dev, test = nli.create_split(["allposWN"]*3,[None,None,None], disjoint=False)
-    model = TfShallowNeuralClassifier(hidden_dim=2, max_iter=1, vocab_dim=2, embedding_dir="embeddings" + hypname, encoder=True)
+    model = TfShallowNeuralClassifier(hidden_dim=hyp[0], max_iter=hyp[1], vocab_dim=hyp[2], hidden_activation=hyp[3], eta=hyp[4], embedding_dir="embeddings" + hypname, encoder=True)
     results = nli.encoder_experiment(train,test,model)
     if results["macro-F1"] > bestF1:
         best_embeddings = "embeddings" + hypname
+        best_params = hyp
+print(best_params)
 
 ratios = [0.5, 0.25, 0.125, 0.06025, 0.030125]
 for ratio in ratios:
     source = str(ratio) + "posWN"
     train, dev, test = nli.create_split([source + ".train", source + ".dev", source + ".test"],[None,None,None], disjoint=False)
-    model = TfShallowNeuralClassifier(hidden_dim=2, max_iter=1, vocab_dim=2, embedding_dir=best_embeddings + ".npy", encoder=False)
+    model = TfShallowNeuralClassifier(hidden_dim=hyp[0], max_iter=hyp[1], vocab_dim=hyp[2], hidden_activation=hyp[3], eta=hyp[4],embedding_dir=best_embeddings + ".npy", encoder=False)
     nli.decoder_experiment(train,test,model)
